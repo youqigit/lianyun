fbff0add2888c03d9fb1e58815c1ec9fd23210ab
youqigit <1qazxsw2QQ>
2018-04-20 13:57:07 +0800

    first

---
+++ WebApplicationGit/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationGit
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WebApplicationGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationGit.MvcApplication" Language="C#" %>
